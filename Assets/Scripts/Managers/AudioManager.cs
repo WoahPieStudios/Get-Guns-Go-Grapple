1e096b9d400c24e16c786831f1de5e328379ce7d
Jaxeetee <66991639+Jaxeetee@users.noreply.github.com>
2022-07-31 16:08:32 +0800

    adding managers

---
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : Singleton<AudioManager> 
+{
+
+    [SerializeField] private AudioSource _musicSource;
+    [SerializeField] private AudioSource _sfxSource;
+
+
+    
+}
